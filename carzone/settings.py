3838c0b572c8252167b3672ea2eb4f846f9ed97b
Muj-taba <e.1p@yahoo.com>
2022-02-02 09:04:36 +0400

    Breaking HTML

--- carzone/settings.py
+++ carzone/settings.py
@@ -119,3 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'carzone/static')
+]
+
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'carzone/static',
+# )
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = (BASE_DIR / 'carzone/static')
